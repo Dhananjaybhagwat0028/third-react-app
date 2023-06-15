36411fd8a993669b6025741f73b4fb9bc4f5a570
dhananjay bhagwat <dhananjaybhagwat777@gmail.com>
2023-06-15 23:26:04 +0530

    string static code

--- react-third-app/src/App.js
+++ react-third-app/src/App.js
@@ -1,17 +1,39 @@
+import { useState } from "react";
 
-import AppBody from "./pages/AppBody";
-import AppFooter from "./pages/AppFooter";
-import AppHeader from "./pages/AppHeader";
-// User Defined Tag: <App />
-function App() {
+function App(){
   return (
     <>
-      <AppHeader />
-      <AppBody />
-      <AppBody />
-      <AppFooter />
+      <h1>
+        String Example
+        <StringDemo />
+      </h1>
     </>
   );
 }
+ function StringDemo (){
+  let [title ,setTitle] = useState("hello world ");
+
+  let lowerCaseAction = () =>{
+    title = title.toLowerCase();
+    console.log(title);
+    //Dom operatons 
+    setTitle(title);
+  };
+
+  let upperCaseAction = () =>{
+    title = title.toUpperCase();
+    console.log(title);
+
+     setTitle(title);
+  };
+
+   return (
+    <>
+    <h1> {title} </h1>
+    <input type="button" value="Lower case" onClick={lowerCaseAction} />
+    <input type="button" value="Upper case" onClick={upperCaseAction} />
+    </>
+   );
+ }
 
 export default App;
